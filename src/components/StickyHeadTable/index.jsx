db8ca3277fb0daa011d98856c382be8a52cf2e2a
Tran Thuy <thuytt0295@gmail.com>
2021-01-17 01:09:23 +0700

    delete dashboard page

--- src/components/StickyHeadTable/index.jsx
+++ src/components/StickyHeadTable/index.jsx
@@ -20,13 +20,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { getMovieSchedule } from "../../redux/actions/movie.action";
 import { nonAccentVietnamese } from "../../utils";
 import { Loader } from "../Loader";
+
 function StickyHeadTable() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
   // set pagination
   const [currentPage, setCurrentPage] = useState(0);
-
 
   const MOVIE_PER_PAGE = 10;
 
@@ -46,11 +46,9 @@ function StickyHeadTable() {
   const { lichChieu } =
     useSelector((state) => state?.movie?.movieScheduleList) || "";
 
-
   const [searchList, setSearchList] = useState(null);
 
   const handleSearch = (e) => {
-    
     let keyword = e.target.value;
     if (keyword) {
       let result = [];
@@ -68,17 +66,15 @@ function StickyHeadTable() {
       }
       // console.log(result);
       setSearchList(result);
-      
-    
     } else {
       setSearchList(null);
     }
   };
 
   const currentPageData = () => {
-    // console.log(searchList); 
+    // console.log(searchList);
     if (searchList) {
-      return searchList.map((item,index) => (
+      return searchList.map((item, index) => (
         <TableRow key={index}>
           <TableCell className="" align="left">
             {item.maLichChieu}
@@ -92,7 +88,7 @@ function StickyHeadTable() {
           <TableCell className="" align="left">
             {item.ngayChieuGioChieu}
           </TableCell>
-       
+
           <TableCell className="" align="left">
             {item.thoiLuong}
           </TableCell>
@@ -102,28 +98,30 @@ function StickyHeadTable() {
         </TableRow>
       ));
     } else {
-      return lichChieu?.slice(offset, offset + MOVIE_PER_PAGE).map((item,index) => (
-        <TableRow key={index}>
-          <TableCell className="" align="left">
-            {item.maLichChieu}
-          </TableCell>
-          <TableCell className="" align="left">
-            {item.thongTinRap.maHeThongRap}
-          </TableCell>
-          <TableCell className="" align="left">
-            {item.thongTinRap.maCumRap}
-          </TableCell>
-          <TableCell className="" align="left">
-            {item.ngayChieuGioChieu}
-          </TableCell>
-          <TableCell className="" align="left">
-            {item.giaVe}
-          </TableCell>
-          <TableCell className="" align="left">
-            {item.thoiLuong}
-          </TableCell>
-        </TableRow>
-      ));
+      return lichChieu
+        ?.slice(offset, offset + MOVIE_PER_PAGE)
+        .map((item, index) => (
+          <TableRow key={index}>
+            <TableCell className="" align="left">
+              {item.maLichChieu}
+            </TableCell>
+            <TableCell className="" align="left">
+              {item.thongTinRap.maHeThongRap}
+            </TableCell>
+            <TableCell className="" align="left">
+              {item.thongTinRap.maCumRap}
+            </TableCell>
+            <TableCell className="" align="left">
+              {item.ngayChieuGioChieu}
+            </TableCell>
+            <TableCell className="" align="left">
+              {item.giaVe}
+            </TableCell>
+            <TableCell className="" align="left">
+              {item.thoiLuong}
+            </TableCell>
+          </TableRow>
+        ));
     }
   };
   const pageCount = Math.ceil(lichChieu?.length / MOVIE_PER_PAGE);
@@ -134,36 +132,33 @@ function StickyHeadTable() {
     setCurrentPage(selectedPage);
   };
 
-
-
-
   if (loading) {
-    return <Loader/>
+    return <Loader />;
   }
 
   return (
     <div style={{ padding: "20px" }}>
-      <Toolbar   handleSearch={handleSearch} />
+      <Toolbar handleSearch={handleSearch} />
       <div style={{ marginTop: "20px" }}>
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell className="">Mã Lich Chiếu</TableCell>
-                <TableCell className="" align="left">
-                  Hệ thông rạp
+                <TableCell style={{ fontWeight: 600 }}>Mã lịch chiếu</TableCell>
+                <TableCell style={{ fontWeight: 600 }} align="left">
+                  Hệ thống rạp
                 </TableCell>
-                <TableCell className="" align="left">
-                  Cụm Rạp
+                <TableCell style={{ fontWeight: 600 }} align="left">
+                  Cụm rạp
                 </TableCell>
-                <TableCell className="" align="left">
+                <TableCell style={{ fontWeight: 600 }} align="left">
                   Ngày giờ chiếu
                 </TableCell>
 
-                <TableCell className="" align="left">
+                <TableCell style={{ fontWeight: 600 }} align="left">
                   Thời lượng phim
                 </TableCell>
-                <TableCell className="" align="left">
+                <TableCell style={{ fontWeight: 600 }} align="left">
                   Giá vé
                 </TableCell>
               </TableRow>
@@ -171,24 +166,30 @@ function StickyHeadTable() {
             <TableBody>{currentPageData()}</TableBody>
           </Table>
         </TableContainer>
-      <div style={{marginTop:"20px",display:"flex",justifyContent:"center"}}>
-      {!searchList ? (
-                <ReactPaginate
-                  previousLabel={<NavigateBeforeIcon />}
-                  nextLabel={<NavigateNextIcon />}
-                  pageCount={pageCount}
-                  onPageChange={handlePageClick}
-                  containerClassName={"pagination"}
-                  previousLinkClassName={"pagination__link"}
-                  nextLinkClassName={"pagination__link"}
-                  disabledClassName={"pagination__link--disabled"}
-                  activeClassName={"pagination__link--active"}
-                  style={{ width: "50%" }}
-                />
-              ) : (
-               ""
-              )}
-      </div>
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {!searchList ? (
+            <ReactPaginate
+              previousLabel={<NavigateBeforeIcon />}
+              nextLabel={<NavigateNextIcon />}
+              pageCount={pageCount}
+              onPageChange={handlePageClick}
+              containerClassName={"pagination"}
+              previousLinkClassName={"pagination__link"}
+              nextLinkClassName={"pagination__link"}
+              disabledClassName={"pagination__link--disabled"}
+              activeClassName={"pagination__link--active"}
+              style={{ width: "50%" }}
+            />
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/Dashboard/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dashboard = () => {
-  return <div>This is Dashboard component</div>;
-};
-
-export default Dashboard;
